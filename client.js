4833ad1e2edb50c9be84e2457fed5f238574bc28
Edward <edward.ascan@me.com>
2021-01-13 23:13:04 +0000

    added connected message on connect

--- client.js
+++ client.js
@@ -10,6 +10,9 @@ const connect = function() {
   });
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
+  conn.on('connect', function() {
+    console.log('Successfully connected to game server');
+  })
   conn.on('data', (data) => {
     console.log("data has come!")
     console.log(data);
