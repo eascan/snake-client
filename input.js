b096c21cc75585097132b4c40fc6354c3e21e503
Edward <edward.ascan@me.com>
2021-01-14 02:09:14 +0000

    wrote movement commands

--- input.js
+++ input.js
@@ -2,8 +2,12 @@
  * Setup User Interface 
  * Specifically, so that we can handle user input via stdin
  */
-const setupInput = function() {
+
+let connection;
+
+const setupInput = function(conn) {
   const stdin = process.stdin;
+  connection = conn;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
@@ -11,6 +15,14 @@ const setupInput = function() {
   const handleUserInput = function(key) {
       if (key === '\u0003') {
         process.exit();
+      } else if (key === 'w') {
+        conn.write("Move: up")
+      } else if (key === 's') {
+        conn.write("Move: down");
+      } else if (key === 'a') {
+        conn.write("Move: left");
+      } else if (key === 'd') {
+        conn.write("Move: right");
       }
     }
   
